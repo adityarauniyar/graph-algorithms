3e958c4e883638a541b18221ea539b1ac9a35206
adityarauniyar <mail.adityarauniyar@gmail.com>
2021-06-21 18:12:21 +0545

    DAG SCC with tests

--- graph-algorithms-tests/DAGStronglyConnectedTests.h
+++ graph-algorithms-tests/DAGStronglyConnectedTests.h
@@ -37,3 +37,70 @@ TEST(DAGStronglyConnectedTests, CASE_3) {
 
 	EXPECT_EQ(4, t.getNumStronglyConnected());
 }
+
+TEST(DAGStronglyConnectedTests, CASE_4) {
+	int n, m;
+	std::vector<std::pair<int, int>> EdgeList;
+	std::istringstream inputStrm("4 4 1 2 4 1 2 3 3 1");
+	inputToVars(inputStrm, n, m, EdgeList);
+
+	Basic::DAGStronglyConnected t(n, m, EdgeList);
+
+	EXPECT_EQ(2, t.getNumStronglyConnected());
+}
+
+TEST(DAGStronglyConnectedTests, CASE_5) {
+	int n, m;
+	std::vector<std::pair<int, int>> EdgeList;
+	std::istringstream inputStrm("5 7 2 1 3 2 3 1 4 3 4 1 5 2 5 3");
+	inputToVars(inputStrm, n, m, EdgeList);
+
+	Basic::DAGStronglyConnected t(n, m, EdgeList);
+
+	EXPECT_EQ(5, t.getNumStronglyConnected());
+}
+
+TEST(DAGStronglyConnectedTests, CASE_6) {
+	int n, m;
+	std::vector<std::pair<int, int>> EdgeList;
+	std::istringstream inputStrm("5 10 5 4 4 5 3 1 1 4 2 3 4 2 3 4 5 2 4 1 3 5");
+	inputToVars(inputStrm, n, m, EdgeList);
+
+	Basic::DAGStronglyConnected t(n, m, EdgeList);
+
+	EXPECT_EQ(1, t.getNumStronglyConnected());
+}
+
+TEST(DAGStronglyConnectedTests, CASE_7) {
+	int n, m;
+	std::vector<std::pair<int, int>> EdgeList;
+	std::istringstream inputStrm("10000 1 1595 7210");
+	inputToVars(inputStrm, n, m, EdgeList);
+
+	Basic::DAGStronglyConnected t(n, m, EdgeList);
+
+	EXPECT_EQ(10000, t.getNumStronglyConnected());
+}
+TEST(DAGStronglyConnectedTests, CASE_8) {
+	int n, m;
+	std::vector<std::pair<int, int>> EdgeList;
+	std::istringstream inputStrm("5 10 5 4 4 5 3 1 1 4 2 3 4 2 3 4 5 2 4 1 3 5");
+	inputToVars(inputStrm, n, m, EdgeList);
+
+	Basic::DAGStronglyConnected t(n, m, EdgeList);
+
+	EXPECT_EQ(1, t.getNumStronglyConnected());
+}
+TEST(DAGStronglyConnectedTests, CASE_9) {
+	int n, m;
+	std::vector<std::pair<int, int>> EdgeList;
+	std::istringstream inputStrm("10 20 5 3 4 10 3 2 1 3 4 9 2 6 8 3 8 2 6 1 6 10 10 6 1 4 3 8 1 5 8 9 5 1 8 5 7 8 3 4 8 4");
+	inputToVars(inputStrm, n, m, EdgeList);
+
+	Basic::DAGStronglyConnected t(n, m, EdgeList);
+
+	EXPECT_EQ(3, t.getNumStronglyConnected());
+}
+
+
+
--- graph-algorithms/DAGStronglyConnected.h
+++ graph-algorithms/DAGStronglyConnected.h
@@ -1,16 +1,23 @@
 #pragma once
 #include"StdGraphDimensions.h"
 #include<vector>
-#include<set>
 
 
 namespace Basic {
 
 	class DAGStronglyConnected : public StandardGraphClass {
 	private:
-		std::vector<bool> VisitedList;
+		enum vertexStatus {
+			FWDVISITED,
+			UNVISITED, 
+			BKDVISITED,
+		};
+
+		std::vector<vertexStatus> VisitedList;
 		std::map<int, std::vector<int>> ReverseAdjacencyList;
+		std::list<int> postOrderList;;
 		int numConnectedGraphgs = 0;
+
 	public:
 		void ExploreDAG(int);
 		void ExploreReverseDAG(int);
@@ -23,30 +30,50 @@ Basic::DAGStronglyConnected::DAGStronglyConnected(int n, int m, std::vector<std:
 	: StandardGraphClass(n, m) {
 
 	for (int i = 0; i <= n; i++)
-		VisitedList.push_back(false);
+		VisitedList.push_back(UNVISITED);
 
 	for (int i = 0; i < EdgeList.size(); i++)
 	{
 		AdjacencyList[EdgeList[i].first].push_back(EdgeList[i].second);
 		ReverseAdjacencyList[EdgeList[i].second].push_back(EdgeList[i].first);
 	}
+	
 
-	if (n > 0)
+	// calculating SCC in DAG
+
+	if (n > 0) {
+
+		// Exploring Reverse DAG
 		for (int i = 1; i <= n; i++) {
-			if (VisitedList[i] == true) continue;
+			if (VisitedList[i] == BKDVISITED) continue;
 			else {
 				ExploreReverseDAG(i);
+				postOrderList.push_front(i);
 			}
 		}
+
+		// Exploring DAG with SCC increaments
+		for (int i = 1; i <= n; i++) {
+			if (VisitedList[postOrderList.front()] == FWDVISITED) {
+				postOrderList.pop_front();
+				continue;
+			}
+			else {
+				++numConnectedGraphgs;
+				ExploreDAG(postOrderList.front());
+				postOrderList.pop_front();
+			}
+		}
+	}
 
 }
 
 void Basic::DAGStronglyConnected::ExploreDAG(int i) {
-	VisitedList[i] = true;
+	VisitedList[i] = FWDVISITED;
 	int itr = 0;
 	while (itr < AdjacencyList[i].size()) {
 
-		if (false == VisitedList[AdjacencyList[i][itr]])
+		if (FWDVISITED != VisitedList[AdjacencyList[i][itr]])
 			ExploreDAG((int)AdjacencyList[i][itr]);
 
 		++itr;
@@ -54,13 +81,14 @@ void Basic::DAGStronglyConnected::ExploreDAG(int i) {
 }
 
 void Basic::DAGStronglyConnected::ExploreReverseDAG(int i) {
-	VisitedList[i] = true;
+	VisitedList[i] = BKDVISITED;
 	int itr = 0;
 	while (itr < ReverseAdjacencyList[i].size()) {
 
-		if (false == VisitedList[ReverseAdjacencyList[i][itr]])
-			ExploreDAG((int)ReverseAdjacencyList[i][itr]);
-
+		if (BKDVISITED != VisitedList[ReverseAdjacencyList[i][itr]]) {
+			ExploreReverseDAG((int)ReverseAdjacencyList[i][itr]);
+			postOrderList.push_front(ReverseAdjacencyList[i][itr]);
+		}
 		++itr;
 	}
 }
